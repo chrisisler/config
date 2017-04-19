2134e96e380ab5c50b1c78f4dcd4d27872671c37
chrisisler <christopherisler1@gmail.com>
2017-04-19 17:03:22 -0400

    2017-04-19 17:03

--- update-all-dotfiles.sh
+++ update-all-dotfiles.sh
@@ -3,3 +3,7 @@
 git add ./*
 git add ./*/.{bashrc,inputrc,tmux.conf,vimrc}
 git commit -m "$(date "+%Y-%m-%d %H:%M")"
+
+if [[ "${?}" != 0 ]]; then
+    printf "All changes added and commited.\n"
+fi
