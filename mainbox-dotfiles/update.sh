72c66021df71248f6a8d28d77988a06d673e7c18
chrisisler <christopherisler1@gmail.com>
2017-04-22 01:13:29 -0400

    2017-04-22 01:13

--- mainbox-dotfiles/update.sh
+++ mainbox-dotfiles/update.sh
@@ -25,6 +25,6 @@ cp ~/.config/ranger/rc.conf .
 cp ~/.config/cmus/solarized.theme .
 
 git add "$(basename "${0}")"
-git add ./*
+git add -u ./*
 git add ./.{bashrc,inputrc,tmux.conf,vimrc}
 git commit -m "$(date "+%Y-%m-%d %H:%M")"
--- update-all-dotfiles.sh
+++ update-all-dotfiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 git add "$(basename "${0}")"
-git add ./*
+git add -u ./*
 git commit -m "$(date "+%Y-%m-%d %H:%M")"
